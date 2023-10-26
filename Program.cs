55451975d47fc592ef44a1bc447e786c3b277fee
Kavabanga000 <labkokosta082@gmail.com>
2023-10-26 08:51:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FunctionValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента x:");
+            double x = double.Parse(Console.ReadLine());
+
+            if (x != 2)
+            {
+                double y = Math.Log(Math.Abs(x - 2));
+                Console.WriteLine($"Значение функции при x = {x} равно {y}");
+            }
+            else
+            {
+                Console.WriteLine($"Недопустимое згачение x ");
+            }
+        }
+    }
+}
